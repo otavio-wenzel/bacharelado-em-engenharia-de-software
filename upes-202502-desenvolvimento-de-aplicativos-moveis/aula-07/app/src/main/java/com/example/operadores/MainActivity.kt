6051ad127b63f741f6d6bebfb01752b4d1a582e0
otavio-wenzel <otavio.wenzel@gmail.com>
2025-08-25 21:18:16 -0300

    Subindo conteudo em kotlin da aula 7 de desenvolvimento mobile

---
+++ upes-202502-desenvolvimento-de-aplicativos-moveis/aula-07/app/src/main/java/com/example/operadores/MainActivity.kt
@@ -0,0 +1,5 @@
+fun main() {
+
+    println("Operadores Lógicos")
+
+}
---
+++ upes-202502-desenvolvimento-de-aplicativos-moveis/aula-07/app/src/main/java/com/example/operadores/While.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    //A estrutura while repete um bloco de código enquanto uma condição for verdadeira.
+
+    var contador = 0
+
+    while (contador < 5) {
+        println("Contagem: $contador")
+        contador++
+    }
+
+}
